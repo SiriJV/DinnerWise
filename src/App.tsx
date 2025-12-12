a6c73b8c169f1702f382bc5b34019bfc6361cd14
jessicaagren <jessicaagren@hotmail.com>
2025-12-12 11:25:26 +0100

    add routing and layout components with Home and Search pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
-import "./App.scss";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './App.scss';
+import RootLayout from './layouts/RootLayout';
+import HomePage from './routes/HomePage';
+import SearchPage from './routes/SearchPage';
 
-function App() {
-  return (
-    <>
-    <h1>Dinnerwise</h1></>
-  );
+// import EventDetailsPage from './routes/BookDetailsPage/BookDetailsPage';
+// import { BooksContextProvider } from './contexts/BooksContext';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <HomePage /> },
+      {
+        path: 'search',
+        element: <SearchPage />,
+      },
+      // {
+      //   path: 'event/:eventid',
+      //   element: <EventDetailsPage />,
+      // },
+    ],
+  },
+]);
+
+export default function App() {
+  return <RouterProvider router={router} />;
 }
-
-export default App;
---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,16 @@
+import { NavLink } from 'react-router-dom';
+import './NavBar.scss';
+
+export default function NavBar() {
+  //   const getLinkClass = ({ isActive }: { isActive: boolean }) =>
+  //     `Link${isActive ? ' active' : ''}`;
+  return (
+    <div className='NavBar__menu'>
+      <nav className='NavBar'>
+        <NavLink to='/'>Home</NavLink>
+
+        <NavLink to='/search'>Search</NavLink>
+      </nav>
+    </div>
+  );
+}
