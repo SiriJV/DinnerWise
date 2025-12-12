627d75a8bc55e7d588dc63f64e2c9d5d5ebec947
jessicaagren <jessicaagren@hotmail.com>
2025-12-12 12:02:42 +0100

    add Footer component with styling and integrate into RootLayout

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import NavBar from '../components/NavBar/NavBar';
+import Footer from '../components/Footer/Footer';
 
 export default function RootLayout() {
   return (
@@ -8,7 +9,7 @@ export default function RootLayout() {
       <main>
         <Outlet />
       </main>
-      <footer>Footer</footer>
+      <Footer />{' '}
     </>
   );
 }
