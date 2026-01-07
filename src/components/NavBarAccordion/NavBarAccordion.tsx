2a3454dd1836275689c7c3289d3f1cda51cb65a9
jessicaagren <jessicaagren@hotmail.com>
2026-01-07 14:12:15 +0100

    map AccordionItems

--- src/components/NavBarAccordion/NavBarAccordion.tsx
+++ src/components/NavBarAccordion/NavBarAccordion.tsx
@@ -1,11 +1,17 @@
-import { Accordion, type AccordionControlProps, Center } from '@mantine/core';
+import { Accordion, type AccordionControlProps } from '@mantine/core';
+import { NavLink } from 'react-router-dom';
+import { accordionItems } from '../../data/NavLinks';
 import './NavBarAccordion.scss';
+
+interface NavBarAccordionProps {
+  onClose?: () => void;
+}
 
 function AccordionControl(props: AccordionControlProps) {
   return <Accordion.Control {...props} />;
 }
 
-export default function NavBarAccordion() {
+export default function NavBarAccordion({ onClose }: NavBarAccordionProps) {
   return (
     <Accordion
       chevronPosition='left'
@@ -19,41 +25,28 @@ export default function NavBarAccordion() {
           margin: 0,
         },
       }}>
-      <Accordion.Item value='item-1'>
-        <AccordionControl>DinnerWise</AccordionControl>
-        <Accordion.Panel>Om oss</Accordion.Panel>
-        <Accordion.Panel className='lastPanel'>Nyhetsbrev</Accordion.Panel>
-      </Accordion.Item>
+      {accordionItems.map((item) => (
+        <Accordion.Item key={item.value} value={item.value}>
+          <AccordionControl>{item.label}</AccordionControl>
 
-      <Accordion.Item value='item-2'>
-        <AccordionControl>Ditt konto</AccordionControl>
-        <Accordion.Panel>Skapa konto</Accordion.Panel>
-        <Accordion.Panel>Logga in</Accordion.Panel>
-        <Accordion.Panel className='lastPanel'>Hjälp</Accordion.Panel>
-      </Accordion.Item>
-
-      <Accordion.Item value='item-3'>
-        <AccordionControl>Hjälp</AccordionControl>
-        <Accordion.Panel>Betalning</Accordion.Panel>
-        <Accordion.Panel>FAQ</Accordion.Panel>
-        <Accordion.Panel>Köpvillkor</Accordion.Panel>
-        <Accordion.Panel className='lastPanel'>
-          Integritetspolicy
-        </Accordion.Panel>
-      </Accordion.Item>
-
-      <Accordion.Item value='item-4'>
-        <AccordionControl>Kontakt</AccordionControl>
-        <Accordion.Panel>Kundservice</Accordion.Panel>
-        <Accordion.Panel className='lastPanel'>Kontakt</Accordion.Panel>
-      </Accordion.Item>
-
-      <Accordion.Item value='item-5'>
-        <AccordionControl>Följ oss</AccordionControl>
-        <Accordion.Panel>Instagram</Accordion.Panel>
-        <Accordion.Panel>Facebook</Accordion.Panel>
-        <Accordion.Panel className='lastPanel'>X</Accordion.Panel>
-      </Accordion.Item>
+          {item.panels.map((panel, index) => (
+            <NavLink
+              key={`/${panel.path}`}
+              to={`${panel.path}`}
+              className={({ isActive }) =>
+                `accordionLink ${isActive ? 'active' : ''}`
+              }
+              onClick={onClose}>
+              <Accordion.Panel
+                className={
+                  index === item.panels.length - 1 ? 'lastPanel' : undefined
+                }>
+                {panel.label}
+              </Accordion.Panel>
+            </NavLink>
+          ))}
+        </Accordion.Item>
+      ))}
     </Accordion>
   );
 }
