b18202b5142935887d215980849f9ec4b0c9cefd
jessicaagren <jessicaagren@hotmail.com>
2026-01-07 12:13:23 +0100

    data for navLinks

---
+++ src/data/NavLinks.tsx
@@ -0,0 +1,9 @@
+export const navLinks = [
+  { label: 'Litteratur och skrivande', to: '/litteratur' },
+  { label: 'Musik', to: '/musik' },
+  { label: 'Natur och friluftsliv', to: '/natur' },
+  { label: 'IT och teknik', to: '/IT' },
+  { label: 'Språk', to: '/sprak' },
+  { label: 'Konst och kultur', to: '/konst' },
+  { label: 'Sport och träning', to: '/sport' },
+];
