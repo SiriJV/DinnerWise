66fc64c62834974294018ca430c65fc23159f05b
jessicaagren <jessicaagren@hotmail.com>
2026-01-07 12:13:56 +0100

    comment out NavBar import in RootLayout

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,11 +1,11 @@
 import { Outlet } from 'react-router-dom';
-import NavBar from '../components/NavBar/NavBar';
+// import NavBar from '../components/NavBarOriginal/NavBarOriginal';
 import Footer from '../components/Footer/Footer';
 
 export default function RootLayout() {
   return (
     <>
-      <NavBar />
+      {/* <NavBar /> */}
       <main>
         <Outlet />
       </main>
