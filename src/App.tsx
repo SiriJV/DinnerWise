5422d504ce2a9a653f2c8b682ca3f07b05bc7a12
jessicaagren <jessicaagren@hotmail.com>
2026-01-07 12:12:46 +0100

    refactor App component to use MantineProvider and RouterProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,33 @@
-// import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-// import './App.scss';
-// import RootLayout from './layouts/RootLayout';
-// import HomePage from './routes/HomePage';
-// import SearchPage from './routes/SearchPage';
-
-// // import EventDetailsPage from './routes/BookDetailsPage/BookDetailsPage';
-// // import { BooksContextProvider } from './contexts/BooksContext';
-
-// const router = createBrowserRouter([
-//   {
-//     path: '/',
-//     element: <RootLayout />,
-//     children: [
-//       { index: true, element: <HomePage /> },
-//       {
-//         path: 'search',
-//         element: <SearchPage />,
-//       },
-//       // {
-//       //   path: 'event/:eventid',
-//       //   element: <EventDetailsPage />,
-//       // },
-//     ],
-//   },
-// ]);
-
-// export default function App() {
-//   return <RouterProvider router={router} />;
-// }
-
 import '@mantine/core/styles.css';
 import { createTheme, MantineProvider } from '@mantine/core';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import AppShell from './components/AppShell/AppShell';
 
-const theme = createTheme({ primaryColor: 'red' });
+const theme = createTheme({
+  primaryColor: 'red',
+});
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <MantineProvider theme={theme}>
+        <AppShell />
+      </MantineProvider>
+    ),
+    children: [
+      { index: true, element: <div>Home</div> },
+      { path: 'litteratur', element: <div>Litteratur och skrivande</div> },
+      { path: 'musik', element: <div>Musik</div> },
+      { path: 'natur', element: <div>Natur och friluftsliv</div> },
+      { path: 'it', element: <div>IT och teknik</div> },
+      { path: 'sprak', element: <div>Språk</div> },
+      { path: 'konst', element: <div>Konst och kultur</div> },
+      { path: 'sport', element: <div>Sport och träning</div> },
+    ],
+  },
+]);
 
 export default function App() {
-  return (
-    <MantineProvider theme={theme}>
-      {/* <MobileNavbar>
-        <div>Här syns innehållet i Main!</div>
-      </MobileNavbar> */}
-      <AppShell>
-        <div>Här syns innehållet i Main!</div>
-      </AppShell>
-    </MantineProvider>
-  );
+  return <RouterProvider router={router} />;
 }
