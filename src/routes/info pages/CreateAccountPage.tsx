71851c28068d33ce875ac4270677d8159ccb699f
jessicaagren <jessicaagren@hotmail.com>
2026-01-08 14:09:19 +0100

    add form for creating account

--- src/routes/info pages/CreateAccountPage.tsx
+++ src/routes/info pages/CreateAccountPage.tsx
@@ -1,7 +1,49 @@
-export default function CreateAccountPage(): React.ReactNode {
+import {
+  Anchor,
+  Container,
+  Group,
+  Paper,
+  PasswordInput,
+  Text,
+  TextInput,
+  Title,
+} from '@mantine/core';
+import BaseButton from '../../components/Buttons/BaseButton/BaseButton';
+
+export default function CreateAccountPage() {
   return (
-    <>
-      <div>Create Account Page</div>
-    </>
+    <Container size={420} my={40}>
+      <Title ta='center'>Skapa konto</Title>
+
+      <Text ta='center'>
+        Redan medlem? <Anchor href='/logga-in'>Logga in</Anchor>
+      </Text>
+
+      <Paper withBorder shadow='sm' p={22} mt={30} radius='xs'>
+        <TextInput
+          label='E-post'
+          placeholder='e-post@dinnerwise.se'
+          required
+          radius='xs'
+        />
+        <PasswordInput
+          label='Lösenord'
+          placeholder='Välj ett lösenord'
+          required
+          mt='md'
+          radius='xs'
+        />
+        <PasswordInput
+          label='Bekräfta lösenord'
+          placeholder='Skriv lösenordet igen'
+          required
+          mt='md'
+          radius='xs'
+        />
+        <BaseButton variantType='primary' fullWidth to='/' mt='lg'>
+          Skapa konto
+        </BaseButton>
+      </Paper>
+    </Container>
   );
 }
