fa77fe4d06a595656cba473705aa593efc1879c7
jessicaagren <jessicaagren@hotmail.com>
2026-01-12 13:39:28 +0100

    add Flex and ImageCarousel

--- src/routes/HomePage/HomePage.tsx
+++ src/routes/HomePage/HomePage.tsx
@@ -1,12 +1,21 @@
-import { Image } from '@mantine/core';
+import { Flex, Image } from '@mantine/core';
 import './HomePage.scss';
 import EventCard from '../../components/EventCard/EventCard';
+import ImageCarousel from '../../components/ImageCarousel/ImageCarousel';
 
 export default function HomePage(): React.ReactNode {
   return (
     <>
       <Image radius='0' h='50vh' src='src\assets\9010.jpg' />
-      <EventCard />
+      <Flex direction='column' gap='md' p='md'>
+        <ImageCarousel />
+        <Flex w='100%' gap='md' justify='space-between'>
+          <EventCard />
+          <EventCard />
+
+          <EventCard />
+        </Flex>
+      </Flex>
     </>
   );
 }
