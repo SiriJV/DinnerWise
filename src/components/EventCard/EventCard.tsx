5374b67a2e1d78893d05ee17139f8796cc307b94
jessicaagren <jessicaagren@hotmail.com>
2026-01-13 14:26:11 +0100

    add demo EventCard

--- src/components/EventCard/EventCard.tsx
+++ src/components/EventCard/EventCard.tsx
@@ -1,13 +1,15 @@
-import { Card, Image, Text, Badge, Button, Group } from '@mantine/core';
+import { Card, Image, Text, Badge, Group } from '@mantine/core';
 
 export default function EventCard() {
   return (
-    <Card shadow='sm' padding='lg' radius='md' withBorder w='31vw'>
+    <Card shadow='sm' padding='lg' radius='md' withBorder>
       <Card.Section>
         <Image
           src='https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png'
           height={160}
           alt='Norway'
+          radius='md'
+          fit='cover'
         />
       </Card.Section>
 
@@ -20,10 +22,6 @@ export default function EventCard() {
         With Fjord Tours you can explore more of the magical fjord landscapes
         with tours and activities on and around the fjords of Norway
       </Text>
-
-      {/* <Button color='blue' fullWidth mt='md' radius='md'>
-        Book classic tour now
-      </Button> */}
     </Card>
   );
 }
