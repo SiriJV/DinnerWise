2536047e8124748a13a2a80d26d1f97c5d0060b0
jessicaagren <jessicaagren@hotmail.com>
2026-01-14 13:13:40 +0100

    add date (startTime and endTime) to props

--- src/routes/HomePage/HomePage.tsx
+++ src/routes/HomePage/HomePage.tsx
@@ -24,8 +24,9 @@ export default function HomePage(): React.ReactNode {
             host='Anders Blom'
             restaurant='Noosh'
             address='Österlånggatan 35, Borås'
-            dateTime={new Date('2026-03-12T18:30:00')}
-            price={99}
+            startTime={new Date('2026-01-19T17:00:00')}
+            endTime={new Date('2026-01-19T18:45:00')}
+            price={150}
             spots={6}
             maxSpots={6}
             description='Den här träffen passar perfekt för dig som vill göra enkla val för en bättre miljö. Vi går igenom olika tips för att bli Zero Waste i vardagen och diskuterar nya trender inom hållbarhetstänk. Hoppas vi ses där!'
@@ -37,7 +38,8 @@ export default function HomePage(): React.ReactNode {
             host='Sara Flinck'
             restaurant='Vinci'
             address='Sandwalls Plats 7A, Borås'
-            dateTime={new Date('2026-03-12T18:30:00')}
+            startTime={new Date('2026-01-24T18:00:00')}
+            endTime={new Date('2026-01-24T20:00:00')}
             price={99}
             spots={3}
             maxSpots={5}
@@ -49,8 +51,9 @@ export default function HomePage(): React.ReactNode {
             host='Roger Göransson'
             restaurant='La Copita'
             address='Allégatan 63, Borås'
-            dateTime={new Date('2026-03-12T18:30:00')}
-            price={99}
+            startTime={new Date('2026-02-04T16:30:00')}
+            endTime={new Date('2026-02-04T18:00:00')}
+            price={120}
             spots={2}
             maxSpots={5}
             description='Vi pratar svenska hiraldriska vapen och deras historia. När kom de till Sverige och hur användes de under århundradena?'
@@ -61,11 +64,12 @@ export default function HomePage(): React.ReactNode {
             host='Patrik Eklund'
             restaurant='Muchacho'
             address='Vasagatan 5A, Göteborg'
-            dateTime={new Date('2026-03-12T18:30:00')}
-            price={99}
+            startTime={new Date('2026-01-22T16:30:00')}
+            endTime={new Date('2026-01-22T17:45:00')}
+            price={149}
             spots={7}
             maxSpots={8}
-            description='.'
+            description='Behöver du ha koll på statistik i Excel? Här går vi tillsammans igenom ett enkelt upplägg och ser på olika funktioner som löser dina problem.'
           />
           <EventCard
             title='Planera din vecka med bullet journaling'
@@ -73,11 +77,12 @@ export default function HomePage(): React.ReactNode {
             host='Kerstin Wallin'
             restaurant='Kalle Glader'
             address='Friggagatan 14B, Göteborg'
-            dateTime={new Date('2026-03-12T18:30:00')}
-            price={99}
+            startTime={new Date('2026-02-03T18:30:00')}
+            endTime={new Date('2026-02-02T19:30:00')}
+            price={85}
             spots={3}
             maxSpots={6}
-            description='.'
+            description='Bullet journaling är ett kul och kreativt sätt att hålla koll på ditt dagliga schema. Tillsammans går vi igenom olika tekniker och hjälps åt med tips och tricks.'
           />
           <EventCard
             title='Studiecirkel: Förstå EU-valet'
@@ -85,8 +90,9 @@ export default function HomePage(): React.ReactNode {
             host='Amir Hassan'
             restaurant='Pacha Grill'
             address='Linnégatan 74, Göteborg'
-            dateTime={new Date('2026-03-12T18:30:00')}
-            price={99}
+            startTime={new Date('2026-01-22T16:30:00')}
+            endTime={new Date('2026-01-22T17:45:00')}
+            price={149}
             spots={5}
             maxSpots={5}
             description='EU påverkar oss alla, men hur ska vi tänka när vi röstar? Vad är viktigt att veta om de olika partierna och deras ståndpunkter?'
@@ -97,11 +103,12 @@ export default function HomePage(): React.ReactNode {
             host='Mia Johansson'
             restaurant='Lilla Tavernan'
             address='Olivedalsgatan 17, Göteborg'
-            dateTime={new Date('2026-03-12T18:30:00')}
-            price={99}
+            startTime={new Date('2026-01-19T17:00:00')}
+            endTime={new Date('2026-01-19T19:00:00')}
+            price={50}
             spots={6}
             maxSpots={7}
-            description='.'
+            description='Vegetarisk mat blir allt mer populär. Men hur ersätter du ditt protein och hur gör du maten extra god utan animalier? Det är lättare än du tror!'
           />
           <EventCard
             title='Grundkurs i urban odling'
@@ -109,11 +116,12 @@ export default function HomePage(): React.ReactNode {
             host='Markus Thede'
             restaurant='Gezana'
             address='Lorensbergsgatan 8, Göteborg'
-            dateTime={new Date('2026-03-12T18:30:00')}
-            price={99}
+            startTime={new Date('2026-01-30T18:00:00')}
+            endTime={new Date('2026-01-30T19:00:00')}
+            price={75}
             spots={8}
             maxSpots={8}
-            description='.'
+            description='Urban odling ger oss alla en möjlighet att odla växter mitt i stan. Vi snackar vilka växter som är bäst lämpade och hur du gör för att få plats i en liten lägenhet.'
           />
         </SimpleGrid>
       </Flex>
