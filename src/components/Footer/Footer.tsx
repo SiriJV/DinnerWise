4c361575d31d30796f2c92773a5e12b95e29a90a
jessicaagren <jessicaagren@hotmail.com>
2026-01-15 14:48:16 +0100

    media queries for mobile

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,40 +2,58 @@ import { Container, Text, Group, Stack, Anchor } from '@mantine/core';
 import { NavLink } from 'react-router-dom';
 import './Footer.scss';
 import { accordionItems } from '../../data/NavLinks';
+import NavBarAccordion from '../NavBarAccordion/NavBarAccordion';
+import { useMediaQuery } from '@mantine/hooks';
 
 export default function Footer() {
+  const isMobile = useMediaQuery('(max-width: 768px)');
+
   return (
     <footer className='footer'>
       <Container size='lg'>
-        <Group align='flex-start' justify='space-between' pb='xl'>
-          <Stack gap='xs' className='footer-brand'>
-            <Text fw={700} size='lg'>
-              DinnerWise
-            </Text>
-            <Text size='xs' c='dimmed'>
-              Små event, stora upplevelser.
-            </Text>
+        {isMobile ? (
+          <Stack gap='md' pb='xl'>
+            <Stack gap='xs' className='footer-brand'>
+              <Text fw={700} size='lg'>
+                DinnerWise
+              </Text>
+              <Text size='xs' c='dimmed'>
+                Små event, stora upplevelser.
+              </Text>
+            </Stack>
+            <NavBarAccordion />
           </Stack>
+        ) : (
+          <Group align='flex-start' justify='space-between' pb='xl'>
+            <Stack gap='xs' className='footer-brand'>
+              <Text fw={700} size='lg'>
+                DinnerWise
+              </Text>
+              <Text size='xs' c='dimmed'>
+                Små event, stora upplevelser.
+              </Text>
+            </Stack>
 
-          <Group className='footer-groups' align='flex-start' wrap='wrap'>
-            {accordionItems.map((group) => (
-              <Stack key={group.value} gap={6} className='footer-group'>
-                <Text fw={600} className='footer-title'>
-                  {group.label}
-                </Text>
+            <Group className='footer-groups' align='flex-start' wrap='wrap'>
+              {accordionItems.map((group) => (
+                <Stack key={group.value} gap={6} className='footer-group'>
+                  <Text fw={600} className='footer-title'>
+                    {group.label}
+                  </Text>
 
-                {group.panels.map((link) => (
-                  <NavLink
-                    key={link.path}
-                    to={link.path}
-                    className='footer-link'>
-                    {link.label}
-                  </NavLink>
-                ))}
-              </Stack>
-            ))}
+                  {group.panels.map((link) => (
+                    <NavLink
+                      key={link.path}
+                      to={link.path}
+                      className='footer-link'>
+                      {link.label}
+                    </NavLink>
+                  ))}
+                </Stack>
+              ))}
+            </Group>
           </Group>
-        </Group>
+        )}
       </Container>
 
       <Container size='lg'>
