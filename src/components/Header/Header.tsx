42af2085c3ab648bebdc999999262e2b57be4bf2
jessicaagren <jessicaagren@hotmail.com>
2026-01-19 09:56:14 +0100

    add icons to Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
-import { Burger, Group, Title, UnstyledButton } from '@mantine/core';
+import { ActionIcon, Burger, Group, UnstyledButton } from '@mantine/core';
 import './Header.scss';
 import { NavLink } from 'react-router-dom';
+import { BellIcon, SearchIcon, UserRound } from 'lucide-react';
 
 interface HeaderProps {
   opened: boolean;
@@ -10,17 +11,41 @@ interface HeaderProps {
 
 export default function Header({ opened, onToggle, onClose }: HeaderProps) {
   return (
-    <Group className='header' h='100%' px='md'>
-      <Burger opened={opened} onClick={onToggle} size='sm' color='white' />
-      <div>
+    <Group className='header' h='100%' w='100%' px='md' justify='space-between'>
+      <Group gap='sm'>
+        <Burger opened={opened} onClick={onToggle} size='sm' color='white' />
+
         <NavLink to='/' onClick={onClose}>
-          <UnstyledButton className='logo'>
-            {' '}
-            {/* <Title order={1}>DinnerWise</Title> */}
-            DinnerWise
-          </UnstyledButton>
+          <UnstyledButton className='logo'>DinnerWise</UnstyledButton>
         </NavLink>
-      </div>
+      </Group>
+
+      <Group gap='md'>
+        <ActionIcon
+          component={NavLink}
+          to='/sokresultat'
+          variant='subtle'
+          color='white'
+          size='md'>
+          <SearchIcon size={20} />
+        </ActionIcon>
+        <ActionIcon
+          component={NavLink}
+          to='/'
+          variant='subtle'
+          color='white'
+          size='md'>
+          <BellIcon size={20} />
+        </ActionIcon>
+        <ActionIcon
+          component={NavLink}
+          to='/profil/:id'
+          variant='subtle'
+          color='white'
+          size='md'>
+          <UserRound size={20} />
+        </ActionIcon>
+      </Group>
     </Group>
   );
 }
