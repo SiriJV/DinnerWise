b69ca747ec4b9c8dc314859456a33484493da79f
SiriJV <siri.viren@icloud.com>
2026-01-20 13:43:04 +0100

    add backend folder

---
+++ backend/src/app.ts
@@ -0,0 +1,29 @@
+// import express from 'express';
+// import restaurantRoutes from './routes/restaurants';
+// import eventsRouter from './routes/events';
+// import categoriesRouter from './routes/categories';
+// import tagsRouter from './routes/tags';
+// import usersRouter from './routes/users';
+// import citiesRouter from './routes/cities';
+// import cors from 'cors';
+
+// const app = express();
+
+// app.use(cors({
+//   origin: 'http://localhost:5175',
+// }));
+
+// app.use(express.json());
+
+// app.use('/restaurants', restaurantRoutes);
+// app.use('/events', eventsRouter);
+// app.use('/categories', categoriesRouter);
+// app.use('/tags', tagsRouter);
+// app.use('/users', usersRouter);
+// app.use('/cities', citiesRouter);
+
+// app.get('/health', (_req, res) => {
+//   res.json({ status: 'ok' });
+// });
+
+// export default app;
