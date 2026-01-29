9da335016819a862c17c86f71d85a871e92d6b7b
jessicaagren <jessicaagren@hotmail.com>
2026-01-29 11:21:11 +0100

    add EventType

---
+++ src/types/EventType.ts
@@ -0,0 +1,13 @@
+export type EventType = {
+  id: number;
+  title: string;
+  description: string;
+  category_id: number;
+  restaurant_id: number;
+  current_participants: number;
+  max_spots: number;
+  price: number;
+  date: string;
+  start_time: string;
+  end_time: string;
+};
