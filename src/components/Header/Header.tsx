ab79475598d7cca3f90fa7eb10cad12dd03c811b
jessicaagren <jessicaagren@hotmail.com>
2026-02-03 14:05:26 +0100

    edit Burger size

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -14,7 +14,14 @@ export default function Header({ opened, onToggle, onClose }: HeaderProps) {
   return (
     <Group className='header' h='100%' w='100%' px='md' justify='space-between'>
       <Group gap='sm'>
-        <Burger opened={opened} onClick={onToggle} size='sm' color='white' />
+        <Burger
+          opened={opened}
+          onClick={onToggle}
+          size='sm'
+          color='white'
+          lineSize={2}
+          aria-label='Toggle navigation'
+        />
 
         <NavLink to='/' onClick={onClose}>
           <UnstyledButton className='logo'>DinnerWise</UnstyledButton>
