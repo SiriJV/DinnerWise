4a4a7351933cb2979814870ef8bde8d0ceee4fa2
jessicaagren <jessicaagren@hotmail.com>
2026-02-03 10:37:59 +0100

    edit styling

--- src/components/ImageCarousel/ImageCarousel.tsx
+++ src/components/ImageCarousel/ImageCarousel.tsx
@@ -19,7 +19,7 @@ export default function NavCarousel() {
           base: '100%',
           sm: '50%',
           md: '33.3333%',
-          lg: '16.6667%',
+          lg: '20%',
         }}
         nextControlIcon={<ChevronRight size={32} />}
         previousControlIcon={<ChevronLeft size={32} />}>
@@ -27,7 +27,7 @@ export default function NavCarousel() {
           <Carousel.Slide key={link.path}>
             <NavLink to={`/${link.path}`} className='navCarousel-link'>
               <Card radius='0' padding={0} className='navCarousel-card'>
-                <Image src={link.image} h={160} fit='cover' radius='md' />
+                <Image src={link.image} h={110} fit='cover' radius='md' />
                 <Text fw='600' className='navCarousel-label'>
                   {link.label}
                 </Text>
