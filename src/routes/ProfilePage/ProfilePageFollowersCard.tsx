c08d5d3527a156da7c3b00353a19b3d8f2e0bae7
jessicaagren <jessicaagren@hotmail.com>
2026-02-12 10:20:04 +0100

    remove unused import of Box

--- src/routes/ProfilePage/ProfilePageFollowersCard.tsx
+++ src/routes/ProfilePage/ProfilePageFollowersCard.tsx
@@ -1,4 +1,4 @@
-import { Text, Group, Stack, Avatar, Box } from '@mantine/core';
+import { Text, Group, Stack, Avatar } from '@mantine/core';
 import FollowBadge from './FollowBadge';
 import { NavLink } from 'react-router-dom';
 
