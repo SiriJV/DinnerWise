cb7e2bf3a50a2005d68904db1030efd5a4a68b42
jessicaagren <jessicaagren@hotmail.com>
2026-02-12 14:31:37 +0100

    add placeholder content and structure to info pages

--- src/routes/info pages/FAQPage.tsx
+++ src/routes/info pages/FAQPage.tsx
@@ -1,7 +1,28 @@
+import { Container, Title, Text } from '@mantine/core';
+
 export default function FAQPage(): React.ReactNode {
   return (
     <>
-      <div>FAQ Page</div>
+      <Container size='lg'>
+        <Title order={2} mb='md'>
+          FAQ{' '}
+        </Title>
+        <Text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed nulla
+          dapibus nisi molestie vehicula. Aenean viverra mauris id diam
+          convallis, et elementum quam aliquet. Curabitur molestie, elit ac
+          maximus consequat, velit turpis gravida est, varius ornare ex turpis
+          at magna. Morbi non erat venenatis, congue enim ut, ullamcorper nulla.
+          Pellentesque et dignissim enim. Phasellus commodo efficitur lobortis.
+          In id accumsan justo, at auctor libero. Nullam mattis lacus facilisis,
+          gravida elit et, imperdiet mi. Duis id mattis massa. Vestibulum vel
+          odio sit amet lorem porta pulvinar. Phasellus pharetra ac turpis a
+          fringilla. Class aptent taciti sociosqu ad litora torquent per conubia
+          nostra, per inceptos himenaeos. Pellentesque gravida ligula sit amet
+          mi egestas, sit amet convallis ipsum suscipit. Maecenas vulputate
+          magna faucibus lorem vehicula pretium.
+        </Text>
+      </Container>
     </>
   );
 }
