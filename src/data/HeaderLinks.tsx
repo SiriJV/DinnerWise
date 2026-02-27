5c3cb16440ff9f0b6feca8cc12c712e4916314b4
jessicaagren <jessicaagren@hotmail.com>
2026-02-27 10:19:51 +0100

    add HeaderLinks with placeholder information

---
+++ src/data/HeaderLinks.tsx
@@ -0,0 +1,13 @@
+export type HeaderLink = {
+  path: string;
+  label: string;
+};
+
+export const headerLinks: HeaderLink[] = [
+  { path: '/', label: 'Menyval 1' },
+  { path: '/', label: 'Menyval 2' },
+  { path: '/', label: 'Menyval 3' },
+  { path: '/', label: 'Menyval 4' },
+  { path: '/', label: 'Menyval 5' },
+  { path: '/', label: 'Menyval 6' },
+];
