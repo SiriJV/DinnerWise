752237c3e1753cb930c032aca76756fede7dc24b
jessicaagren <jessicaagren@hotmail.com>
2026-03-04 11:02:04 +0100

    add variants to BookMarkIcon

--- src/components/BookmarkIcon/BookmarkIcon.tsx
+++ src/components/BookmarkIcon/BookmarkIcon.tsx
@@ -1,31 +1,66 @@
-import { Box } from '@mantine/core';
+import { Box, Flex } from '@mantine/core';
 import { BookmarkIcon } from 'lucide-react';
 import { useAuth } from '../../contexts/AuthContext';
 
 interface BookmarkButtonProps {
   eventId: number;
+  variant?: 'lg' | 'sm';
 }
 
-export default function BookmarkButton({ eventId }: BookmarkButtonProps) {
+export default function BookmarkButton({
+  eventId,
+  variant = 'lg',
+}: BookmarkButtonProps) {
   const { bookmarks, removeBookmark, addBookmark } = useAuth();
   const isBookmarked = bookmarks.includes(eventId);
 
   const handleBookmarkClick = (e: React.MouseEvent) => {
     e.preventDefault();
     e.stopPropagation();
+
     if (isBookmarked) removeBookmark(eventId);
     else addBookmark(eventId);
   };
 
+  if (variant === 'sm') {
+    return (
+      <Box
+        onClick={handleBookmarkClick}
+        style={{
+          position: 'absolute',
+          top: 12,
+          right: 12,
+          cursor: 'pointer',
+          background: 'white',
+          borderRadius: 999,
+          padding: 8,
+          boxShadow: '0 2px 8px rgba(0,0,0,0.08)',
+        }}>
+        <BookmarkIcon
+          size={18}
+          color='black'
+          fill={isBookmarked ? 'black' : 'none'}
+        />
+      </Box>
+    );
+  }
+
   return (
-    <Box
-      className={`bookmarkButton ${isBookmarked ? 'bookmarked' : ''}`}
+    <Flex
+      px='md'
+      py='sm'
+      bg='rgba(206, 212, 218, 1)'
+      bdrs='sm'
+      style={{
+        cursor: 'pointer',
+        width: 'fit-content',
+      }}
       onClick={handleBookmarkClick}>
       <BookmarkIcon
-        size={18}
+        size={22}
         color='black'
         fill={isBookmarked ? 'black' : 'none'}
       />
-    </Box>
+    </Flex>
   );
 }
