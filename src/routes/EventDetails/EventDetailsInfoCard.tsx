df40542014dad115f47ae3438d57f2d8564092a6
jessicaagren <jessicaagren@hotmail.com>
2026-03-09 10:56:43 +0100

    fix: remove unused media query and simplify text size in EventDetailsInfoCard

--- src/routes/EventDetails/EventDetailsInfoCard.tsx
+++ src/routes/EventDetails/EventDetailsInfoCard.tsx
@@ -1,5 +1,4 @@
 import { Text, Stack, Box } from '@mantine/core';
-import { useMediaQuery } from '@mantine/hooks';
 
 type EventDetailsInfoCardProps = {
   title: string;
@@ -10,17 +9,11 @@ export default function EventDetailsInfoCard({
   title,
   content,
 }: EventDetailsInfoCardProps) {
-  const isMobile = useMediaQuery('(max-width: 768px)');
-
   return (
-    <Box
-      p='md'
-      bdrs='md'
-      // bd='1px solid rgba(206, 212, 218, 1)'
-      bg='rgba(255, 204, 199, 1)'>
+    <Box p='md' bdrs='md' bg='rgba(255, 204, 199, 1)'>
       <Stack align='center' gap={4}>
-        <Text size={isMobile ? 'xs' : 'sm'}>{title}</Text>
-        <Text size={isMobile ? 'xs' : 'sm'} fw={600} ta='center'>
+        <Text size={'sm'}>{title}</Text>
+        <Text size={'sm'} fw={600} ta='center'>
           {content}
         </Text>
       </Stack>
