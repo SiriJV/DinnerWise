75edc7a012ac9110b6c17fcbb3c98cb9e78a3736
jessicaagren <jessicaagren@hotmail.com>
2026-03-17 15:14:13 +0100

    add HowToPage content and styling for event guidelines

--- src/routes/info pages/HowToPage/HowToPage.tsx
+++ src/routes/info pages/HowToPage/HowToPage.tsx
@@ -1,7 +1,17 @@
-import { Container, Title, Text, Stepper, Stack, Grid } from '@mantine/core';
+import {
+  Container,
+  Title,
+  Text,
+  Stepper,
+  Stack,
+  Grid,
+  List,
+  ThemeIcon,
+} from '@mantine/core';
 import { useEffect, useRef, useState } from 'react';
 import FloatingActionButton from '../../../components/FAB/FAB';
 import './HowToPage.scss';
+import { CircleCheckBig, OctagonX } from 'lucide-react';
 
 export default function HowToPage() {
   const [active, setActive] = useState(0);
@@ -43,109 +53,130 @@ export default function HowToPage() {
   }, []);
 
   return (
-    <>
-      <Container size='lg'>
-        <FloatingActionButton />
+    <Container size='lg' p='md'>
+      <FloatingActionButton />
 
-        <Title order={2} mb='md'>
-          Att tänka på när du skapar ett event
-        </Title>
+      <Title order={2} mb='md'>
+        Att tänka på när du skapar ett event
+      </Title>
+
+      <Stack gap='xl'>
+        <Text>
+          Att arrangera ett event är ett fantastiskt sätt att träffa nya
+          människor, dela kunskap och skapa gemenskap. Tänk på att ditt event
+          ska vara tillgängligt, roligt och tryggt för alla deltagare. Här får
+          du några tips och riktlinjer för att lyckas!
+        </Text>
+
         <Stack>
-          <Text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed
-            nulla dapibus nisi molestie vehicula. Aenean viverra mauris id diam
-            convallis, et elementum quam aliquet. Curabitur molestie, elit ac
-            maximus consequat, velit turpis gravida est, varius ornare ex turpis
-            at magna. Morbi non erat venenatis, congue enim ut, ullamcorper
-            nulla. Pellentesque et dignissim enim. Phasellus commodo efficitur
-            lobortis. In id accumsan justo, at auctor libero. Nullam mattis
-            lacus facilisis, gravida elit et, imperdiet mi. Duis id mattis
-            massa. Vestibulum vel odio sit amet lorem porta pulvinar. Phasellus
-            pharetra ac turpis a fringilla. Class aptent taciti sociosqu ad
-            litora torquent per conubia nostra, per inceptos himenaeos.
-            Pellentesque gravida ligula sit amet mi egestas, sit amet convallis
-            ipsum suscipit. Maecenas vulputate magna faucibus lorem vehicula
-            pretium.
-          </Text>
-          <Stack>
-            <Title order={3}>Riktlinjer</Title>
-            <Text>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Necessitatibus libero alias, recusandae amet laudantium non
-              doloremque sequi veniam impedit obcaecati, quidem repellendus
-              vitae molestias. Veniam quaerat perferendis architecto illo
-              similique.
-            </Text>
-            <Text>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Necessitatibus libero alias, recusandae amet laudantium non
-              doloremque sequi veniam impedit obcaecati, quidem repellendus
-              vitae molestias. Veniam quaerat perferendis architecto illo
-              similique.
-            </Text>
-            <Text>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Necessitatibus libero alias, recusandae amet laudantium non
-              doloremque sequi veniam impedit obcaecati, quidem repellendus
-              vitae molestias. Veniam quaerat perferendis architecto illo
-              similique.
-            </Text>
-          </Stack>
-          <Stack>
-            <Title order={3}>Hur går det till?</Title>
-
-            <Grid gutter='md'>
-              <Grid.Col span={4}>
-                <Stepper
-                  active={active}
-                  onStepClick={(step) => {
-                    setActive(step);
-                    stepRefs[step].current?.scrollIntoView({
-                      behavior: 'smooth',
-                      block: 'center',
-                    });
-                  }}
-                  orientation='vertical'>
-                  <Stepper.Step
-                    label='Steg 1'
-                    description='Skapa konto'
-                    className='howto-step'
-                  />
-                  <Stepper.Step
-                    label='Steg 2'
-                    description='Skapa event'
-                    className='howto-step'
-                  />
-                  <Stepper.Step label='Steg 3' description='Publicera event' />
-                </Stepper>
-              </Grid.Col>
-              <Grid.Col span={8}>
-                <Stack h='100%' justify='space-between'>
-                  <Text ref={stepRefs[0]}>
-                    Skapa eller logga in på ett konto. För att kunna skapa ett
-                    event behöver du vara inloggad. Detta är för att säkerställa
-                    att du är den du säger dig vara och för att kunna hantera
-                    dina event och bokningar på ett smidigt och säkert sätt.
-                  </Text>
-                  <Text ref={stepRefs[1]} style={{ alignSelf: 'center' }}>
-                    Skapa eventet. Tryck på plustecknet i översta högra hörnet
-                    på sidan eller på den flytande plusknappen i nedre högra
-                    hörnet. Följ instruktionerna, fyll i dina uppgifter och
-                    skapa eventet. Nu är det bara att vänta!
-                  </Text>
-                  <Text ref={stepRefs[2]}>
-                    Eventet publiceras när det har godkänts av DinnerWise och
-                    när restaurangen har accepterat bokningen. Då kan du se ditt
-                    event på sidan och andra användare kan anmäla sig till det.
-                    Tips är att dela ditt event på sociala medier för att få
-                    fler deltagare.
-                  </Text>
-                </Stack>
-              </Grid.Col>
-            </Grid>
-          </Stack>
+          <Title order={3}>Riktlinjer för värdar</Title>
+          <List
+            spacing='md'
+            size='sm'
+            center
+            icon={
+              <ThemeIcon size={24} radius='xl' color='rgba(36, 56, 33, 1)'>
+                <CircleCheckBig size={16} />
+              </ThemeIcon>
+            }>
+            <List.Item className='list-item'>
+              Välj ett tydligt ämne eller aktivitet. Det kan vara allt från
+              matlagning, språk, bokdiskussioner till hobbyprojekt. Tänk på att
+              det ska vara något som lockar deltagarna och är roligt att
+              utforska tillsammans.
+            </List.Item>
+            <List.Item
+              className='list-item'
+              icon={
+                <ThemeIcon size={24} radius='xl'>
+                  <OctagonX size={16} />
+                </ThemeIcon>
+              }
+              c='red'>
+              DinnerWise godkänner inte event som är av politisk, religiös eller
+              kommersiell natur, eller som på annat sätt kan uppfattas som
+              stötande eller olämpliga.
+            </List.Item>
+            <List.Item className='list-item'>
+              Var välkomnande och tydlig som värd. Hjälp deltagarna att känna
+              sig inkluderade och trygga, särskilt i en liten grupp där alla kan
+              bidra till samtalet.
+            </List.Item>
+            <List.Item className='list-item'>
+              Se till att eventet är anpassat efter gruppen och lokalen. Mötet
+              sker över en måltid, så tänk på att välja en restaurang eller
+              lokal som passar för det antal deltagare du har och den typ av
+              aktivitet ni ska göra.
+            </List.Item>
+            <List.Item className='list-item'>
+              Uppmuntra positiv interaktion. Hjälp deltagarna att samarbeta,
+              ställa frågor och dela sina erfarenheter utan press.
+            </List.Item>
+            <List.Item className='list-item'>
+              Skicka eventuell förhandsinformation. Om deltagarna behöver ta med
+              något eller förbereda sig på ett visst sätt, ge tydlig och vänlig
+              instruktion innan eventet.
+            </List.Item>
+          </List>
         </Stack>
-      </Container>
-    </>
+
+        <Stack>
+          <Title order={3}>Hur går det till?</Title>
+
+          <Grid gutter='md'>
+            <Grid.Col span={4}>
+              <Stepper
+                active={active}
+                onStepClick={(step) => {
+                  setActive(step);
+                  stepRefs[step].current?.scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'center',
+                  });
+                }}
+                orientation='vertical'>
+                <Stepper.Step
+                  label='Steg 1'
+                  description='Skapa konto'
+                  className='howto-step'
+                />
+                <Stepper.Step
+                  label='Steg 2'
+                  description='Skapa event'
+                  className='howto-step'
+                />
+                <Stepper.Step label='Steg 3' description='Publicera event' />
+              </Stepper>
+            </Grid.Col>
+
+            <Grid.Col span={8}>
+              <Stack h='100%' justify='space-between'>
+                <Text ref={stepRefs[0]}>
+                  Skapa eller logga in på ett konto. Det behövs för att du ska
+                  kunna hantera dina event, kommunicera med deltagare och få
+                  notifieringar om bokningar. Att ha ett konto gör också att
+                  deltagarna kan känna sig trygga.
+                </Text>
+
+                <Text ref={stepRefs[1]} style={{ alignSelf: 'center' }}>
+                  Skapa ditt event. Välj ett namn, beskrivning och antal
+                  platser. Ange restaurang eller lokal, datum och tid. Glöm inte
+                  att lägga till tydliga instruktioner för deltagarna om vad de
+                  behöver ta med sig eller förbereda.
+                </Text>
+
+                <Text ref={stepRefs[2]}>
+                  När eventet är klart att publiceras, dubbelkolla all
+                  information. När det har godkänts av DinnerWise och
+                  restaurangen har bekräftat bokningen blir det synligt för
+                  deltagare. Tips: Dela eventet på sociala medier och i
+                  relevanta grupper för att få fler deltagare.
+                </Text>
+              </Stack>
+            </Grid.Col>
+          </Grid>
+        </Stack>
+      </Stack>
+    </Container>
   );
 }
