dcd5a59f4b8df7ec750d1f85da0306b4aff3f53d
jessicaagren <jessicaagren@hotmail.com>
2026-03-17 12:18:25 +0100

    update HeaderLinks path

--- src/data/HeaderLinks.tsx
+++ src/data/HeaderLinks.tsx
@@ -6,7 +6,7 @@ export type HeaderLink = {
 export const headerLinks: HeaderLink[] = [
   { path: '/stad/goteborg', label: 'Göteborg' },
   { path: '/stad/boras', label: 'Borås' },
-  { path: '/att-skapa-event', label: 'Skapa event' },
+  { path: '/att-skapa-event', label: 'Att skapa event' },
   { path: '/om-oss', label: 'Om oss' },
   { path: '/nyhetsbrev', label: 'Nyhetsbrev' },
 ];
