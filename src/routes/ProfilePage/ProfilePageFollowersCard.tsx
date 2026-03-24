68cc47c3dfe06f3e01e1ae1f9c639a05ffbd7ffb
jessicaagren <jessicaagren@hotmail.com>
2026-03-24 12:34:19 +0100

    add hover style on followersCard on profile page

--- src/routes/ProfilePage/ProfilePageFollowersCard.tsx
+++ src/routes/ProfilePage/ProfilePageFollowersCard.tsx
@@ -31,7 +31,7 @@ export default function ProfilePageFollowersCard({
           gap: '12px',
         }}>
         <Avatar src={profile_picture_url} bd='1px solid gray.5'></Avatar>
-        <Stack gap='0' flex={1}>
+        <Stack gap='0' flex={1} className='link-hover'>
           <Text fw={600}>{alias}</Text>
           <Text>{name}</Text>
         </Stack>
