94cf311bf98e245d5f0b0ef3af39d3e04124d846
jessicaagren <jessicaagren@hotmail.com>
2026-03-25 10:54:35 +0100

    refactor: replace BaseButton with Mantine Button in EventActions

--- src/routes/EventDetails/EventActions.tsx
+++ src/routes/EventDetails/EventActions.tsx
@@ -1,6 +1,5 @@
-import { Box, Grid, Group, Text } from '@mantine/core';
+import { Box, Button, Grid, Group, Text } from '@mantine/core';
 import { FlagIcon, Share } from 'lucide-react';
-import BaseButton from '../../components/Buttons/BaseButton/BaseButton';
 import BookmarkButton from '../../components/BookmarkIcon/BookmarkIcon';
 
 type Props = {
@@ -31,9 +30,9 @@ export default function EventActions({
 
       <Grid.Col span={{ base: 12, md: 6 }} order={{ base: 1, md: 2 }}>
         <Group w='100%' align='stretch' gap='md' wrap='wrap'>
-          <BaseButton size='md' style={{ flex: 1 }} onClick={register.open}>
+          <Button size='md' style={{ flex: 1 }} onClick={register.open}>
             {isFull ? 'Skriv upp dig på väntelista' : 'Anmäl dig här'}
-          </BaseButton>
+          </Button>
 
           {isLoggedIn && <BookmarkButton eventId={eventId} variant='lg' />}
           <Box
