8fcbd8b36dec7aa7ab2d15f421446e7ac20bf674
jessicaagren <jessicaagren@hotmail.com>
2026-04-28 13:53:29 +0200

    feat: update FooterFULogo component to include link wrapping and improve image handling

--- src/components/Footer/FooterFULogo.tsx
+++ src/components/Footer/FooterFULogo.tsx
@@ -1,5 +1,6 @@
 import { Image } from '@mantine/core';
 import FUlogoKVGbgsmaller from '../../assets/FUlogos/FUlogoKVGbgsmaller.png';
+import { Link } from 'react-router-dom';
 
 interface FooterFULogoProps {
   hiddenFromSmall: Boolean;
@@ -8,30 +9,33 @@ interface FooterFULogoProps {
 export default function FooterFULogo({
   hiddenFromSmall = true,
 }: FooterFULogoProps) {
+  const FULink = 'https://www.folkuniversitetet.se/';
   if (hiddenFromSmall) {
     return (
-      <Image
-        mt={'md'}
-        hiddenFrom='sm'
-        src={FUlogoKVGbgsmaller}
-        alt='Swish'
-        w={'auto'}
-        mah={30}
-        fit='contain'
-      />
+      <Link to={FULink}>
+        <Image
+          mt={'md'}
+          hiddenFrom='sm'
+          src={FUlogoKVGbgsmaller}
+          alt='Folkuniversitetet logo'
+          w={'200'}
+          mah={30}
+          fit='contain'
+        />
+      </Link>
     );
   }
 
   return (
-    <Image
-      mt={'md'}
-      visibleFrom='sm'
-      src={FUlogoKVGbgsmaller}
-      alt='Swish'
-      w={'auto'}
-      mah={50}
-      fit='contain'
-      mr={'xl'}
-    />
+    <Link to={FULink}>
+      <Image
+        mt={'md'}
+        visibleFrom='sm'
+        src={FUlogoKVGbgsmaller}
+        alt='Folkuniversitetet logo'
+        w={'220'}
+        fit='contain'
+      />
+    </Link>
   );
 }
