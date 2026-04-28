ccb2daba455a673759c7241e3cc7f3b5f8987b80
jessicaagren <jessicaagren@hotmail.com>
2026-04-28 11:20:06 +0200

    feat: add payment option logos and update import paths in PaymentModal component

--- src/components/Modals/EventModals/PaymentModal/PaymentModal.tsx
+++ src/components/Modals/EventModals/PaymentModal/PaymentModal.tsx
@@ -11,8 +11,8 @@ import {
   Button,
 } from '@mantine/core';
 import { CreditCard } from 'lucide-react';
-import swishLogo from '../../../../assets/swish-logo.png';
-import klarnaLogo from '../../../../assets/klarna-logo.webp';
+import swishLogo from '../../../../assets/payment-options/swish-logo.png';
+import klarnaLogo from '../../../../assets/payment-options/klarna-logo.webp';
 import type { EventType } from '../../../../types/EventType';
 import './PaymentModal.scss';
 import RegisteringBaseModal from '../../RegisteringBaseModal/RegisteringBaseModal';
