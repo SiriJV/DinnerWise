ba2ccb7705342ffa453e864bd4a372eb51357036
SiriJV <siri.viren@icloud.com>
2026-04-29 15:37:41 +0200

    stuff

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,8 @@ import app from './app.js';
 
 const PORT = 3001;
 
+console.log('[server.ts] ========== NEW BACKEND START (TIMESTAMP: ' + new Date().getTime() + ') ==========');;
+
 app.listen(PORT, () => {
-  console.log(`API running on http://localhost:${PORT}`);
+  console.log(`[server.ts] API running on http://localhost:${PORT}`);
 });
